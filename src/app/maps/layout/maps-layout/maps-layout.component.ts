bf11e6aff0cdda55dfda5318018eb50f39859dce
David Hernandez <davidmiguelh@gmail.com>
2023-11-21 00:08:13 -0600

    Fix token mapbox_key and refres token laster by disable its

--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -3,7 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRtaWd1ZWxoIiwiYSI6ImNscDdoMDB0YTF4azYya3F2Y21wM3FzaXUifQ.ZkaVcSB79H6VuGqg1gLfdw';
+import { environment } from '../../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapbox_key;
 
 @Component({
   standalone: true,
